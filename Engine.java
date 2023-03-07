527d8d1f7b30fa7c5ba47634ca1b573fcf521612
Eva Gittardi <egittardi@smith.edu>
2023-03-07 18:09:31 -0500

    A5 Final

--- Engine.java
+++ Engine.java
@@ -4,9 +4,9 @@ public class Engine {
      */
     private FuelType fuel_type;
     private double maximum_fuel_level;
-    public double current_fuel_level;
+    private double current_fuel_level;
 
-    public Engine(FuelType fuel_type, double maximum_fuel_level, double current_fuel_level){
+    public Engine(FuelType fuel_type, double maximum_fuel_level){
         /** 
          * Contains variables for the type of fuel, the maximum fuel level, and the train's current fuel level.
          * @param fuel_type The train's fuel type.
@@ -15,12 +15,29 @@ public class Engine {
          */
         this.fuel_type = fuel_type;
         this.maximum_fuel_level = maximum_fuel_level;
-        this.current_fuel_level = current_fuel_level;
+        this.current_fuel_level = maximum_fuel_level;
+    }
+
+    public double getMaximum_fuel_level() {
+        /**
+         * Getter for the maximum fuel level method.
+         * @return the maximum fuel level of the train.
+         */
+        return this.maximum_fuel_level;
+    }
+
+    public double getCurrent_fuel_level() {
+        /**
+         * Getter for the current fuel level method.
+         * @return The current fuel level of the train.
+         */
+        return this.current_fuel_level;
     }
 
     public void go() {
         /** 
          * Simulates the train's fuel level depleting over time. Throws an exception when the train's current fuel level reaches 0 to end the method.
+         * @throws RuntimeException When the train runs out of fuel.
          */
             if (this.current_fuel_level > 0) {
                 this.current_fuel_level--;
@@ -35,8 +52,8 @@ public class Engine {
         /**
          * Refuels the train, setting the train's current fuel level to its maximum.
          */
-        //make boolean statement for when current fuel level is already equal to the max fuel level?
         this.current_fuel_level = this.maximum_fuel_level;
     }
+
 
 }
--- Train.java
+++ Train.java
@@ -1,30 +1,118 @@
+import java.util.ArrayList;
+
 public class Train {
     /**
      * Ties Car, Engine, and Passenger classes together.
      * @param args The program's arguments.
      */
-    public static void main(String[] args) {
+    private final Engine engine;
+    private ArrayList<Car> Cars_currently_attached;
 
-        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0, 15.0);
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int maximum_capacity) {
+        /**
+        * The train with its fuel details, number of cars, and amount of the people that can fit in the train.
+        * @param fuelType The type of fuel the train is using.
+        * @param fuelCapacity The amount of fuel the train can hold.
+        * @param nCars The number of cars attached to the train.
+        * @param maximum_capacity The maximum amount of people that can fit in a train car.
+        */
+        this.engine = new Engine(fuelType, fuelCapacity);
+        this.Cars_currently_attached = new ArrayList<Car>();
+        for (int i=0; i<nCars; i++) {
+            this.Cars_currently_attached.add(new Car(maximum_capacity));
+        }
+    }
+
+    public Engine getEngine() {
+        /**
+        * The getter for the Engine class.
+        * @return The methods within the Engine class.
+        */
+        
+        return this.engine;
+    }
+    public Car getCar(int i) {
+        /**
+        * The getter for the Car class.
+        * @throws RuntimeException When the value of i exceeds the amount of cars currently attached to the train.
+        * @return The cars currently attached to the train.
+        */
+        if (i>Cars_currently_attached.size()) {
+            throw new RuntimeException("'i' value is out of range! The train doesn't have this many cars. It actually has" + Cars_currently_attached.size() + "cars .");
+        }
+        return this.Cars_currently_attached.get(i);
+    }
+
+    public int getMaxCapacity() {
+        /**
+        * The getter for the getMaxCapacity method.
+        * @return The maximum capacity of all the train cars.
+        */
+        int getMaxCapacity_value = 0; 
+        for (int i=0; i<Cars_currently_attached.size(); i++) {
+            getMaxCapacity_value = getMaxCapacity_value + Cars_currently_attached.get(i).getCapacity();
+        }
+        return getMaxCapacity_value;
+    }
+
+    public int seatsRemaining() {
+         /**
+        * The getter for the getMaxCapacity method.
+        * @return The seats remaining in the train car.
+        */
+        int seatsRemaining_value = 0; 
+        for (int i=0; i<Cars_currently_attached.size(); i++) {
+            seatsRemaining_value = seatsRemaining_value + Cars_currently_attached.get(i).seatsRemaining();
+        }
+        return seatsRemaining_value;
+    }
+
+    public void printManifest() {
+        /**
+         * Prints the names of all the passengers onboard each train car.
+         */
+        for (int i=0; i<this.Cars_currently_attached.size(); i++){ 
+            Cars_currently_attached.get(i).printManifest();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Train newTrain = new Train(FuelType.ELECTRIC, 100.0, 3, 15);
+        newTrain.Cars_currently_attached.add(new Car(10));
         try {
             while (true) {
-                myEngine.go();
+                newTrain.engine.go();
             }
         } catch (Exception e) {
-            System.err.println(e.getMessage()); // Out of fuel (what is this line supposed to do after throwing the exception in engine.java?)
+            System.err.println(e.getMessage());
         }
-        Car my_first_car = new Car(null, 10);
-        // Passenger bob = new Passenger("Billy Bob Sr."); 
-        // my_first_car.addPassenger(bob);
-        // System.out.println("Passenger added.");
-        // my_first_car.printManifest();
-        // my_first_car.removePassenger(bob);
-        // System.out.println("Passenger kicked out.");
-        // my_first_car.printManifest();
+        newTrain.engine.refuel();
+        System.out.println(newTrain.engine.getCurrent_fuel_level());
+
+        Passenger bob = new Passenger("Billy Bob Sr.");
+        Passenger bobjr = new Passenger("Billy Bob jr");
+
+        newTrain.Cars_currently_attached.get(0).addPassenger(bob);
+        newTrain.Cars_currently_attached.get(1).addPassenger(bobjr);
+        newTrain.Cars_currently_attached.get(1).removePassenger(bobjr);
+        System.out.println(newTrain.getMaxCapacity());
+        System.out.println(newTrain.seatsRemaining());
+        newTrain.printManifest();
+
+        Car my_first_car = new Car(10);
+        bobjr.boardCar(my_first_car);
+        bobjr.boardCar(my_first_car); // will throw the exception where passenger is already onboard car
+        bobjr.getOffCar(my_first_car);
+        bobjr.getOffCar(my_first_car); // will throw the exception where passenger is not onboard car, so can't be removed from there.
+        my_first_car.addPassenger(bobjr);
+        my_first_car.printManifest();
+        my_first_car.removePassenger(bobjr);
+        System.out.println("Passenger kicked out.");
+        my_first_car.printManifest();
         
     }
 
     
 
 }
-
